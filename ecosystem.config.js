4a35415337bceb4d26cf06990962b770551ef21a
hmota <heliomotabr@gmail.com>
2026-02-03 10:44:22 -0300

    Disable pingslack app in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [
   { name   : "trabalhista", script : "./t.js",  stop_exit_codes: [0] },
-  { name   : "pingslack", script : "./ping-slack.js"},
+  // { name   : "pingslack", script : "./ping-slack.js"},
   ]
 }
